f612e37e0b52da042fd9dc8ad5c3e38438d101d3
Lewis Liu <lewisl9029@users.noreply.github.com>
2025-08-11 18:42:00 -0700

    Scope platform binaries under @lewisl9029

--- dl.sh
+++ dl.sh
@@ -6,13 +6,13 @@ tgz="$dir/esbuild-$ESBUILD_VERSION.tgz"
 
 # Download the binary executable for the current platform
 case $platform in
-  'Darwin arm64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/darwin-arm64/-/darwin-arm64-$ESBUILD_VERSION.tgz";;
-  'Darwin x86_64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/darwin-x64/-/darwin-x64-$ESBUILD_VERSION.tgz";;
-  'Linux arm64' | 'Linux aarch64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/linux-arm64/-/linux-arm64-$ESBUILD_VERSION.tgz";;
-  'Linux x86_64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/linux-x64/-/linux-x64-$ESBUILD_VERSION.tgz";;
-  'NetBSD amd64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/netbsd-x64/-/netbsd-x64-$ESBUILD_VERSION.tgz";;
-  'OpenBSD arm64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/openbsd-arm64/-/openbsd-arm64-$ESBUILD_VERSION.tgz";;
-  'OpenBSD amd64') curl -fo "$tgz" "https://registry.npmjs.org/@esbuild/openbsd-x64/-/openbsd-x64-$ESBUILD_VERSION.tgz";;
+  'Darwin arm64') curl -fo "$tgz" "https://registry.npmjs.org/@lewisl9029/esbuild-darwin-arm64/-/esbuild-darwin-arm64-$ESBUILD_VERSION.tgz";;
+  'Darwin x86_64') curl -fo "$tgz" "https://registry.npmjs.org/@lewisl9029/esbuild-darwin-x64/-/esbuild-darwin-x64-$ESBUILD_VERSION.tgz";;
+  'Linux arm64' | 'Linux aarch64') curl -fo "$tgz" "https://registry.npmjs.org/@lewisl9029/esbuild-linux-arm64/-/esbuild-linux-arm64-$ESBUILD_VERSION.tgz";;
+  'Linux x86_64') curl -fo "$tgz" "https://registry.npmjs.org/@lewisl9029/esbuild-linux-x64/-/esbuild-linux-x64-$ESBUILD_VERSION.tgz";;
+  'NetBSD amd64') curl -fo "$tgz" "https://registry.npmjs.org/@lewisl9029/esbuild-netbsd-x64/-/esbuild-netbsd-x64-$ESBUILD_VERSION.tgz";;
+  'OpenBSD arm64') curl -fo "$tgz" "https://registry.npmjs.org/@lewisl9029/esbuild-openbsd-arm64/-/esbuild-openbsd-arm64-$ESBUILD_VERSION.tgz";;
+  'OpenBSD amd64') curl -fo "$tgz" "https://registry.npmjs.org/@lewisl9029/esbuild-openbsd-x64/-/esbuild-openbsd-x64-$ESBUILD_VERSION.tgz";;
   *) echo "error: Unsupported platform: $platform"; exit 1
 esac
 
